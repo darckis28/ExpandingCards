9324fdf0f653c8977fd9eb1149050d90a4917398
darckis <aguirre280912@gmail.com>
2023-05-24 18:46:04 -0500

    proyect

---
+++ index.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll(".panel")
+panels.forEach((panel)=> {
+    panel.addEventListener("click",()=>{
+        removeClass();
+        panel.classList.add("active")
+    })
+});
+const removeClass=()=>{
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
